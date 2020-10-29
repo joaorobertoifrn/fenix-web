490ea421d7aac8276c67330997803a0aa5e0b09e
João Roberto <joao.roberto.pereira@accenture.com>
2020-10-29 15:04:50 -0300

    Correção no server,js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
+const port = process.env.port || 3000;
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
@@ -9,4 +10,6 @@ app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log("Servidor Rodando na porta : "+ port);
+});
